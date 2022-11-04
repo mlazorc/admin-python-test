179dd6166ec1fd07b1d6e14bfc8950a6bcecb5be
mlazorc <mlazo@andain.cl>
2022-11-04 18:55:34 -0300

    Se agrega archivo para instalación de modulo

--- Test/Scripts/test_cargaVencmiento.py
+++ Test/Scripts/test_cargaVencmiento.py
@@ -2,10 +2,13 @@ import time
 
 from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
-
+from selenium.webdriver.common.by import By
 from Pages.PageObjects.CargarVencimientosPage import CargarVencimientosPage
 from Pages.PageObjects.LoginPage import LoginPage
 from Pages.TestBase.Funciones import Funciones
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common import TimeoutException
 
 
 def test_acceso_vista():
@@ -38,5 +41,35 @@ def test_carga_masiva_vencimientos():
     carga = CargarVencimientosPage(driver)
     carga.accesoVista(2)
     carga.cargaVencimientos("/Users/sebastiandelvillar/Downloads/test_carga_3.csv", "Octubre", "2022", .5)
+    parrafo = driver.find_element(By.XPATH, "//*[@class='modal-text']")
+    texto = "Se ha ingresado una solicitud de carga. Cuando el archivo este listo, se enviará un correo de notificación."
+    assert parrafo.text().equals(texto), "No fue posible realizar carga"
+    print("Test 2: Carga de vencimientos realizada con éxito")
     time.sleep(5)
     driver.quit()
+
+def test_archivoCarga_invalido():
+    navegador = Service("../../utils/chromedriver")
+    driver = webdriver.Chrome(service=navegador)
+    f = Funciones(driver)
+    f.Navegar("http://certificacion.qaandain.oneapp.cl/admin", 2)
+    driver.maximize_window()
+    loginPage = LoginPage(driver)
+    loginPage.accesoLogin("admin", "andain5546")
+    carga = CargarVencimientosPage(driver)
+    carga.accesoVista(2)
+    carga.cargaVencimientos("/Users/sebastiandelvillar/Downloads/GC-13741_CP.xlsx", "Octubre", "2022", .5)
+    time.sleep(5)
+    try:
+        parrafo = driver.find_element(By.XPATH, "//*[@class='modal-text']")
+        text = "No ha sido posible realizar la carga por los siguientes motivos:"
+        val = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, parrafo)))
+        val = driver.find_element(By.XPATH, parrafo)
+        assert val.text() == text, "Prueba fallida"
+        print("Test 3: Carga archivo formato incorrecto")
+        print("Prueba OK")
+        driver.quit()
+    except TimeoutException as error:
+        print(error.msg)
+        print("No se encontro el elemento {}".format(parrafo.text))
+        driver.quit()
