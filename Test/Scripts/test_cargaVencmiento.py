81bc2e78490339244d9209b666abf5d2b4b42e8e
mlazorc <106356276+mlazorc@users.noreply.github.com>
2022-11-09 15:05:06 -0300

    Update test_cargaVencmiento.py

--- Test/Scripts/test_cargaVencmiento.py
+++ Test/Scripts/test_cargaVencmiento.py
@@ -16,7 +16,7 @@ def test_acceso_vista():
     options = Options()
     options.add_argument("--headless")
     options.add_argument("--window-size=1920x1080")
-    navegador = Service("/var/jenkins_home/workspace/admin-test/chromedriver")
+    navegador = Service("./chromedriver")
     driver = webdriver.Chrome(service=navegador, options = options)
     f = Funciones(driver)
     f.Navegar("http://certificacion.qaandain.oneapp.cl/admin", 2)
