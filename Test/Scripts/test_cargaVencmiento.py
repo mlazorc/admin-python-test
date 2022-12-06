0acbe25d98f60d92458967dd20d6a9f6f9f34822
mlazorc <106356276+mlazorc@users.noreply.github.com>
2022-12-06 14:58:42 -0300

    Update test_cargaVencmiento.py

--- Test/Scripts/test_cargaVencmiento.py
+++ Test/Scripts/test_cargaVencmiento.py
@@ -18,7 +18,7 @@ def test_acceso_vista():
     options.add_argument('--headless')
     options.binary_location = '/usr/bin/google-chrome'
     navegador = Service('/usr/local/bin/chromedriver')
-    driver = webdriver.Chrome(executable_path='../../utils/chromedriver', options=options)
+    driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver', options=options)
     f = Funciones(driver)
     f.Navegar("http://certificacion.qaandain.oneapp.cl/admin", 2)
     driver.maximize_window()
