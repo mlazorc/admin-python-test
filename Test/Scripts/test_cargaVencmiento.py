df4c4386c91e8b4e04056d39bc529ebde1dc1503
mlazorc <106356276+mlazorc@users.noreply.github.com>
2022-12-08 08:33:24 -0300

    Update test_cargaVencmiento.py

--- Test/Scripts/test_cargaVencmiento.py
+++ Test/Scripts/test_cargaVencmiento.py
@@ -18,7 +18,6 @@ def test_acceso_vista():
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
-    options.binary_location = '/usr/local/bin/chromedriver'
     service = Service('/usr/local/bin/chromedriver')
     driver = webdriver.Chrome(service = service, options=options)
     f = Funciones(driver)
