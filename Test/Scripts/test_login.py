35b81099f603ab4b25fcf930e62f94c27bf3dd65
tricomarc <mlazo@andain.cl>
2022-12-14 11:15:57 -0300

    Test 9

--- Test/Scripts/test_login.py
+++ Test/Scripts/test_login.py
@@ -40,7 +40,7 @@ def test_correct_login(user, clave):
         assert sitio == url, "No fue posible acceder al sitio"
         driver.save_screenshot("/var/jenkins_home/workspace/admin-pipeline/Evidencia/fake_login.png")
         print("Test 1: Credenciales correctas")
-        print("Prueba OK 5")
+        print("Prueba OK 6")
     except AssertionError as msg:
         print(msg)
     driver.quit()
