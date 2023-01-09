5184cdcdd98d16d7776e2ff8ab32a8aef0b39e96
Marco Lazo <mlazorc@gmail.com>
2023-01-09 09:38:51 -0300

    Validación formulario Login

--- forms.py
+++ forms.py
@@ -1,13 +1,23 @@
 from wtforms import Form
-from wtforms import StringField
+from wtforms import StringField, TextAreaField
 from wtforms import validators
+
+
+def lenght_honeypot(form, field):
+    if len(field.data) > 0:
+        raise validators.ValidationError('El campo debe estar vacio')
 
 class CommentForm(Form):
     username = StringField('Usuario',
         [ 
-           validators.DataRequired(message='El usuario es requerido'),
+           validators.DataRequired(message='El ingreso de usuario es oblogatorio.'),
            validators.length(min=4, max=25, message='Ingrese nombre de usuario válido')
         ]
         )
-    clave = StringField('Clave')
+    clave = StringField('Clave',
+        [
+            validators.DataRequired(message='El ingreso de clave es obligatorio.')
+        ]
+        )
     nombre_completo = StringField('Nombre Completo')
+    honeyPot = TextAreaField('', [lenght_honeypot])
